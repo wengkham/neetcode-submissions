1a2e86a0421e551523c9f351d53d20e49bcd6c50
wk <39992484+wengkham@users.noreply.github.com>
2026-06-19 11:17:27 +0800

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.py
@@ -0,0 +1,28 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack += [val]
+
+    def pop(self) -> None:
+        # n = self._getStackLastIdx() - 1
+        # del self.stack[n]
+        self.stack[:] = self.stack[:-1]
+
+    def top(self) -> int:
+        n = self._getStackLastIdx() - 1
+        return self.stack[n]
+
+    def getMin(self) -> int:
+        smallest = self.stack[0]
+        for num in self.stack:
+          if num < smallest:
+            smallest = num
+        return smallest  
+        # return min(self.stack)
+
+    def _getStackLastIdx(self) -> int:
+        return len(self.stack)
+        
